7229008acb46668e62dd739ab728229253a92fdf
pablo-rank <50866155+pablo-rank@users.noreply.github.com>
2021-05-05 15:30:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -16,16 +16,16 @@ self.addEventListener('install', (event) => {
 //   );
 // });
 
-self.addEventListener('activate', event => {
-  console.log('V1 now ready to handle fetches!');
-});
+// self.addEventListener('activate', event => {
+//   console.log('V1 now ready to handle fetches!');
+// });
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
+// self.addEventListener('fetch', event => {
+//   const url = new URL(event.request.url);
 
-  // serve the horse SVG from the cache if the request is
-  // same-origin and the path is '/dog.svg'
-  if (url.origin == location.origin && url.pathname == '/dog.svg') {
-    event.respondWith(caches.match('/horse.svg'));
-  }
-});
+//   // serve the horse SVG from the cache if the request is
+//   // same-origin and the path is '/dog.svg'
+//   if (url.origin == location.origin && url.pathname == '/dog.svg') {
+//     event.respondWith(caches.match('/horse.svg'));
+//   }
+// });
