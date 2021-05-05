ed021a78cd0c3341034856da434cd88db90646b6
pablo-rank <50866155+pablo-rank@users.noreply.github.com>
2021-05-05 16:58:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
 import('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-window.prod.mjs');
-//import {cacheNames} from 'workbox-core';
 
 self.addEventListener('install', (event) => {
   console.log('2- V1 installing…');
@@ -8,25 +7,99 @@ self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
 });
 
-// self.addEventListener('install', event => {
-//   console.log('V1 installing…');
 
-//   // cache a horse SVG
-//   event.waitUntil(
-//     caches.open('static-v1').then(cache => cache.add('/horse.svg'))
-//   );
-// });
+const OFFLINE_HTML = 'offline.html';
+const PRECACHE = [
+    { url: OFFLINE_HTML },
+    { url: 'images/fox-1.jpg' },
+    { url: 'images/fox-2.jpg' },
+    { url: 'images/fox-3.jpg' },
+    { url: 'images/fox-4.jpg' },
+    { url: 'images/fox-5.jpg' },
+];
 
-// self.addEventListener('activate', event => {
-//   console.log('V1 now ready to handle fetches!');
-// });
+/**
+ * Precache Manifest for resources available offline.
+ * https://developers.google.com/web/tools/workbox/modules/workbox-precaching#explanation_of_the_precache_list
+ */
+workbox.precaching.precacheAndRoute(PRECACHE);
 
-// self.addEventListener('fetch', event => {
-//   const url = new URL(event.request.url);
+/**
+ * Enable navigation preload.
+ */
+workbox.navigationPreload.enable();
 
-//   // serve the horse SVG from the cache if the request is
-//   // same-origin and the path is '/dog.svg'
-//   if (url.origin == location.origin && url.pathname == '/dog.svg') {
-//     event.respondWith(caches.match('/horse.svg'));
-//   }
-// });
+/**
+ * Runtime caching for CSS and JS coming from only from the origin
+ * https://developers.google.com/web/tools/workbox/modules/workbox-strategies#advanced_usage
+ */
+/* workbox.routing.registerRoute(
+  ({ sameOrigin, url }) => sameOrigin && url.pathname.match(/\.(?:js|css)/),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'origin-js-css'
+  })
+); */
+
+/**
+ * Google Fonts caching
+ * Learn more: https://developers.google.com/web/tools/workbox/guides/common-recipes#google_fonts
+ */
+/**
+ * Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+ */
+workbox.routing.registerRoute(
+    /https:\/\/fonts\.googleapis\.com\/*/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'google_fonts_stylesheets'
+    }),
+    'GET'
+);
+/**
+ * Cache the underlying font files with a cache-first strategy for 1 year.
+ */
+workbox.routing.registerRoute(
+    /https:\/\/fonts\.gstatic\.com\/*/,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google_fonts_webfonts',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            }),
+            new workbox.expiration.ExpirationPlugin({
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+                maxEntries: 30
+            })
+        ]
+    }),
+    'GET'
+);
+
+/**
+ * Basic caching for images.
+ */
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                // Only cache 80 images.
+                maxEntries: 80,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+/*
+ * Fallback to offline HTML page when a navigation request fails.
+ */
+const htmlHandler = new workbox.strategies.NetworkOnly();
+// A NavigationRoute matches navigation requests in the browser, i.e. requests for HTML.
+const navigationRoute = new workbox.routing.NavigationRoute(({ event }) => {
+    const request = event.request;
+    return htmlHandler.handle({ event, request }).catch(() => caches.match(OFFLINE_HTML, {
+        ignoreSearch: true
+    }));
+});
+workbox.routing.registerRoute(navigationRoute);
