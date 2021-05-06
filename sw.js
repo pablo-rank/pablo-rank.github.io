397490b9610923f447a7e330bef1fa48272b3c71
pablo-rank <50866155+pablo-rank@users.noreply.github.com>
2021-05-06 15:12:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ addEventListener('message', (event) => {
 });
 
 console.log("new sw");
+console.log("test");
 
 // importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-window.prod.mjs');
 
